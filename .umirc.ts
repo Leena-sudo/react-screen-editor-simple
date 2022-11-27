2faa8394be7f72a0792ddd79f9883a46288aa547
zhangna <zhangna@360.cn>
2022-11-27 09:53:28 +0800

    feat: init

--- .umirc.ts
+++ .umirc.ts
@@ -8,6 +8,18 @@ export default defineConfig({
   alias: {
     src: './src',
   },
+  base: '/react-screen-editor-simple/',
+  publicPath: '/react-screen-editor-simple/',
+  apiParser: {
+    propFilter: {
+      // 是否忽略从 node_modules 继承的属性，默认值为 false
+      skipNodeModules: true,
+      // 需要忽略的属性名列表，默认为空数组
+      skipPropsWithName: [],
+      // 是否忽略没有文档说明的属性，默认值为 false
+      skipPropsWithoutDoc: false,
+    },
+  },
   // title: 'react-screen-editor-simple',
   // favicon: '/react-screen-editor-simple/editor_logo.png',
   // logo: '/react-screen-editor-simple/editor_logo.png',
